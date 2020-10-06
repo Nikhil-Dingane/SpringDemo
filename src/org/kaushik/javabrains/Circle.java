bdc604624c44d7925f5803125be3a53d2d7df059
Nikhil Dingane <nikhil@krishagni.com>
2020-10-06 09:51:01 +0530

    removing ambiquity by using @Qualifier and qualifier tag

--- src/org/kaushik/javabrains/Circle.java
+++ src/org/kaushik/javabrains/Circle.java
@@ -2,7 +2,6 @@ package org.kaushik.javabrains;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Required;
 
 public class Circle implements Shape {
 	
